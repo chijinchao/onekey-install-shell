76dab63a6f03d3bb8dd46b6df66c677c8d6813dc
chijinchao <chijinchao@gmail.com>
2019-04-30 14:06:59 +0800

    version rollback
    ssssss-off-by: chijinchao <chijinchao@gmail.com>

--- frps/version.sh
+++ frps/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export FRPS_VER=0.25.0
+export FRPS_VER=0.24.1
 export FRPS_INIT="https://raw.githubusercontent.com/chijinchao/onekey-install-shell/master/frps/frps.init"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
